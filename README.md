b09cc2b4683c48783ca791dc08b75cf156953868
benroom <45122905+benroom@users.noreply.github.com>
2023-02-11 19:11:23 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# aboutme
+githubページのソース置き場です。↓のリンクが本体です。
+https://benroom.github.io/aboutme/
---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+    $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
